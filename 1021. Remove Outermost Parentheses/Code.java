0347134041d693cdff0871504f298ac3d4eadfdb
rohit8866seervi <rohitseervi8866@gmail.com>
2026-01-14 11:12:13 +0530

    1021. Remove Outermost Parentheses

---
+++ 1021. Remove Outermost Parentheses/Code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder ans=new StringBuilder();
+
+        for(char c:s.toCharArray()){
+            if(c=='(') {
+                if(count>0) ans.append(c);
+                count++;
+            }
+            else {
+                count--;
+                if(count>0) ans.append(c);
+            }
+            
+        }
+        return ans.toString();
+    }
+}
