2b63be1996fa686fac0e2c23078e79c1c44b62b5
rohit8866seervi <rohitseervi8866@gmail.com>
2026-01-14 09:01:16 +0530

    189. Rotate Array

---
+++ 189. Rotate Array/Code.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void rotate(int[] arr, int d) {
+        int m=d%arr.length;
+        int i=0;
+        int j=arr.length-m-1;
+        while(i<=j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        i=arr.length-m;
+        j=arr.length-1;
+        while(i<=j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        i=0;
+        j=arr.length-1;
+        while(i<=j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
