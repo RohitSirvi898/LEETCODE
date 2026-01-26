a66335a9d927c9b852411b500c73f596f42cc139
rohit8866seervi <rohitseervi8866@gmail.com>
2026-01-26 21:20:28 +0530

    50. Pow(x, n)

---
+++ 50. Pow(x, n)/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        
+        if (N < 0) {
+            N = -N;
+            x = 1 / x;
+        }
+        
+        return helper(x, N);
+    }
+
+    private double helper(double x, long n) {
+        if (n == 0) return 1;
+        
+        double halfpow = helper(x, n / 2);
+        double result = halfpow * halfpow;
+        if (n % 2 == 1) result *= x;
+        
+        return result;
+    }
+}
