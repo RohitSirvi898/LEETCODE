23582d880713c2efadd23ffb43dc98cd39871d81
rohit8866seervi <rohitseervi8866@gmail.com>
2026-02-05 08:38:17 +0530

    1380. Lucky Numbers in a Matrix

---
+++ 1380. Lucky Numbers in a Matrix/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int min=Integer.MAX_VALUE;
+            int colidx=-1;
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]<min){
+                    min=matrix[i][j];
+                    colidx=j;
+                }
+            }
+            boolean isLucky = true;
+            for(int j=0;j<n;j++){
+                if(matrix[j][colidx]>min) isLucky=false;
+            }
+            if(isLucky) ans.add(min);
+        }
+        
+        return ans;
+    }
+}
