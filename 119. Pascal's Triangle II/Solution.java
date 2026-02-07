f0cdc8de0b3410e0e42d7150069bca3273671d13
rohit8866seervi <rohitseervi8866@gmail.com>
2026-02-07 10:02:39 +0530

    119. Pascal's Triangle II

---
+++ 119. Pascal's Triangle II/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans = new ArrayList<>();
+        long prev=1;
+        ans.add(1);
+        for(int i=1;i<rowIndex;i++){
+            long curr=prev*(rowIndex-i+1)/i;
+            ans.add((int)curr);
+            prev=curr;
+        }
+        if(rowIndex>0) ans.add(1);
+        return ans;
+    }
+}
--- 138. Copy List with Random Pointer/Solution.java
+++ 138. Copy List with Random Pointer/Solution.java
@@ -1,3 +1,42 @@
-public class Solution {
-    
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null) return head;
+        Node temp = head;
+        while(temp!=null){
+            Node newNode = new Node(temp.val);
+            newNode.next=temp.next;
+            temp.next=newNode;
+            temp=temp.next.next;
+        }
+        temp=head;
+        while(temp!=null){
+            if(temp.random!=null) temp.next.random=temp.random.next;
+            temp=temp.next.next;
+        }
+        Node newHead=head.next;
+        Node newtemp=newHead;
+        temp=head;
+        while(temp!=null){
+            temp.next=temp.next.next;
+            temp=temp.next;
+            if (newtemp.next!=null) newtemp.next=newtemp.next.next;
+            newtemp=newtemp.next;
+        }
+        return newHead;
+    }
 }
