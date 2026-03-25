f383a397f0023419e51792ac6322cd69c9dcc7a9
rohit8866seervi <rohitseervi8866@gmail.com>
2026-03-25 17:07:14 +0530

    45. Jump Game II

---
+++ 45. Jump Game II/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int far = 0;
+        int near = 0;
+        int jump = 0;
+        while(far<n-1){
+            int farthest = 0;
+            for(int i=near;i<=far;i++){
+                farthest = Math.max(farthest,i+nums[i]);
+            }
+            near = far+1;
+            far = farthest;
+            jump++;
+        }
+        return jump;
+    }
+}
