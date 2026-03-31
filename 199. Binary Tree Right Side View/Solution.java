5d46e0dfc54a4c02fd6a4b22538b723e742e6282
rohit8866seervi <rohitseervi8866@gmail.com>
2026-03-31 12:22:36 +0530

    199. Binary Tree Right Side View

---
+++ 199. Binary Tree Right Side View/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        traversal(root, 0);
+        return ans;
+    }
+
+    public void traversal(TreeNode node, int level){
+        if(node==null) return;
+        if(ans.size()==level) ans.add(node.val);
+        traversal(node.right, level+1);
+        traversal(node.left, level+1);
+    }
+}
