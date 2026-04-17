493149b5ec6014ca5451b7cc5f07c1be9119190c
rohit8866seervi <rohitseervi8866@gmail.com>
2026-04-17 20:42:28 +0530

    455. Assign Cookies

---
+++ 455. Assign Cookies/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int j = 0;
+        int count = 0;
+        for(int i=0;i<s.length;i++){
+            if(j<g.length && g[j]<=s[i]){
+                count++;
+                j++;
+            }
+        }
+        return count;
+    }
+}
