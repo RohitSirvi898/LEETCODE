a327492c12dd700d897de6b43a5033a0bebd8d5a
rohit8866seervi <rohitseervi8866@gmail.com>
2026-05-29 11:44:43 +0530

    69. Sqrt(x)

---
+++ 69. Sqrt(x)/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0;
+        int h = x;
+        while(l<=h){
+            int m = l+(h-l)/2;
+            if((long)m*m>x) h=m-1;
+            else l = m+1;
+        }
+        return h;
+    }
+}
