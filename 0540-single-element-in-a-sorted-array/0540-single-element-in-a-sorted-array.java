175854e526f24cfbf783ed946b18f96fb4d9e88b
Rohit Sirvi <rohitseervi8866@gmail.com>
2026-06-02 10:59:11 +0530

    Time: 0 ms (100%), Space: 52.8 MB (75.17%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,13 +6,12 @@ class Solution {
         int h=n-1;
         while(l<h){
             int m = l+(h-l)/2;
-            // if(nums[m]!=nums[m-1] && nums[m]!=nums[m+1]) return nums[m];
             if(m%2==0){
-                if(m<n-1 && nums[m]!=nums[m+1]) h=m;
+                if( nums[m]!=nums[m+1]) h=m;
                 else l=m+1;
             }
             else{
-                if(m>0 && nums[m]!=nums[m-1]) h=m;
+                if( nums[m]!=nums[m-1]) h=m;
                 else l=m+1;
             }
         }
