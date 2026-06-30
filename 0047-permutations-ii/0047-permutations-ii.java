cd2c7caa5d6c15d2291ef30c296e5898b01638fd
Rohit Sirvi <rohitseervi8866@gmail.com>
2026-06-30 09:43:25 +0530

    Time: 3 ms (90.64%), Space: 47 MB (19.74%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,31 +1,26 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        helper(nums,0);
+        boolean vis[] = new boolean[nums.length];
+        helper(nums,new ArrayList<>(),vis);
         return ans;
     }
 
-    public void helper(int[] nums, int idx){
-        if(idx==nums.length){
-            List<Integer> temp = new ArrayList<>();
-            for(int num:nums) temp.add(num);
-            ans.add(temp);
+    public void helper(int[] nums, List<Integer> temp,boolean[] vis){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-
         HashSet<Integer> set = new HashSet<>();
-        
-        for(int i=idx;i<nums.length;i++){
-            if(set.contains(nums[i])) continue;
-            set.add(nums[i]);
-            swap(nums,i,idx);
-            helper(nums,idx+1);
-            swap(nums,i,idx);
+        for(int i=0;i<nums.length;i++){
+            if(!vis[i] && !set.contains(nums[i])){
+                set.add(nums[i]);
+                vis[i]=true;
+                temp.add(nums[i]);
+                helper(nums,temp,vis);
+                temp.removeLast();
+                vis[i] = false;
+            }
         }
-    }
-    public void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
