96f42cd1e99e44edce24526e365d597114dad075
Rohit Sirvi <rohitseervi8866@gmail.com>
2026-06-30 08:54:01 +0530

    Time: 1 ms (87.3%), Space: 45.7 MB (16.4%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,25 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        helper(nums,0);
+        List<Integer> temp = new ArrayList<>();
+        boolean vis[] = new boolean[nums.length];
+        helper(nums,temp,vis);
         return ans;
     }
 
-    public void helper(int[] nums, int idx){
-
-        if(idx==nums.length){
-            List<Integer> temp = new ArrayList<>();
-            for(int num:nums) temp.add(num);
-            ans.add(temp);
+    public void helper(int[] nums, List<Integer> temp,boolean[] vis){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=idx;i<nums.length;i++){
-            swap(nums,i,idx);
-            helper(nums,idx+1);
-            swap(nums,i,idx);
+        for(int i=0;i<nums.length;i++){
+            if(!vis[i]){
+                vis[i]=true;
+                temp.add(nums[i]);
+                helper(nums,temp,vis);
+                temp.removeLast();
+                vis[i] = false;
+            }
         }
-    }
-    public void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
